4e1bb110e564c816f0da19c2fce8bce1df04d481
Shawn Ma <shawnma@gmail.com>
2018-10-22 21:43:03 -0700

    Don't know exactly..

--- inspector/inspector_tank/inspector_tank.ino
+++ inspector/inspector_tank/inspector_tank.ino
@@ -2,71 +2,117 @@
 #include <EnableInterrupt.h>
 #include <AFMotor.h>
 
+AF_DCMotor left(3);
+AF_DCMotor right(4);
+#define COUNT  2
+class Transformer;
+Transformer* transformers[COUNT];
+bool turning = false;
+
 class Transformer {
   public:
-    Transformer(uint8_t in, int out): _in(in), motor(out), dir(0) {
+    Transformer(uint8_t in): _in(in) {
       pinMode(in, INPUT_PULLUP);
-
-      Serial.println(String("Attach ") + in + "/" + out);
     }
   private:
-    volatile uint16_t start;
     uint8_t _in;
-    volatile uint16_t current;
-    AF_DCMotor motor;
-    int8_t dir;
+    volatile uint16_t start;
   public:
     inline int getIn() {
       return _in;
     }
-    virtual void interrupt() {
+    bool interrupt() {
       if (digitalRead(_in) == HIGH) {
         start = micros();
-        //Serial.println("interrupt");
       } else {
         uint16_t length = (uint16_t)(micros() - start);
         if (abs(length - current) > 20) {
           current = length;
-          //servo.writeMicroseconds(ms);
           setSpeed();
         }
       }
+      return false;
     }
-
-    void setSpeed() {
-      if (current > 1550) {
-        //int speed = map(current, 1520, 2000, 128, 255);
-        int speed = 255;
-        if (dir != 1) {
-          dir = 1;
-          motor.run(FORWARD);
-        }
-        set(speed);
-      } else if (current < 1450) {
-        //int speed = map(current, 1480, 1000, 128, 255);
-        int speed = 255;
-        if (dir != -1) {
-          dir = -1;
-          motor.run(BACKWARD);
-        }
-        set(speed);
-      } else {
-        if (dir != 0) {
-          dir = 0;
-          motor.run(RELEASE);
-        }
-      }
-    }
-
-    void set(uint16_t speed) {
-      //Serial.println(String("::") + _in + "|"+ current + " -->" + speed);
-      motor.setSpeed(speed);
-    }
-
+    volatile uint16_t current;
+    virtual void setSpeed() = 0;
 };
 
-#define COUNT  2
-Transformer* transformers[COUNT];
+class Turner : public Transformer {
+  public:
+    Transformer* mover;
+    Turner(uint8_t in, Transformer* mover) : Transformer(in), mover(mover) {}
+    virtual void setSpeed() {
+      if (current > 1550 || current < 1450) {
+        turning = true;
+        left.setSpeed(255);
+        right.setSpeed(255);
+        if (current > 1550) {
+          left.run(FORWARD);
+          right.run(BACKWARD);
+        } else {
+          left.run(BACKWARD);
+          right.run(FORWARD);
+        }
+      } else {
+        turning = false;
+        mover->setSpeed(); // mover
+      }
+    }
+};
+
+class Mover : public Transformer {
+  public:
+    Mover(uint8_t in) : Transformer(in) {}
+    virtual void setSpeed() {
+      if (turning) {
+        return;
+      }
+      left.setSpeed(255);
+      right.setSpeed(255);
+      if (current > 1550) {
+        left.run(FORWARD);
+        right.run(FORWARD);
+      } else if (current < 1450) {
+        left.run(BACKWARD);
+        right.run(BACKWARD);
+      } else {
+        left.run(RELEASE);
+        right.run(RELEASE);
+      }
+    }
+};
+//    void setSpeed() {
+//      if (current > 1550) {
+//        //int speed = map(current, 1520, 2000, 128, 255);
+//        int speed = 255;
+//        if (dir != 1) {
+//          dir = 1;
+//          motor.run(FORWARD);
+//        }
+//        set(speed);
+//      } else if (current < 1450) {
+//        //int speed = map(current, 1480, 1000, 128, 255);
+//        int speed = 255;
+//        if (dir != -1) {
+//          dir = -1;
+//          motor.run(BACKWARD);
+//        }
+//        set(speed);
+//      } else {
+//        if (dir != 0) {
+//          dir = 0;
+//          motor.run(RELEASE);
+//        }
+//      }
+//    }
+//
+//    void set(uint16_t speed) {
+//      //Serial.println(String("::") + _in + "|"+ current + " -->" + speed);
+//      motor.setSpeed(speed);
+//    }
+//
+//};
+
 
 void int_func() {
   for (int i = 0; i < COUNT; i++) {
@@ -77,18 +123,15 @@ void int_func() {
 }
 
 void setup() {
-  Serial.begin(115200);
-  transformers[0] = new Transformer(9, 1);
-  transformers[1] = new Transformer(10, 2);
-  for (int i = 0; i < COUNT; i++) {
+  //Serial.begin(115200);
+  transformers[0] = new Mover(9);
+  transformers[1] = new Turner(10, transformers[0]);
+  for (int i = 0; i < 2; i++) {
     enableInterrupt(transformers[i]->getIn(), int_func, CHANGE);
   }
 }
 
-// In the loop we just display interruptCount. The value is updated by the interrupt routine.
 void loop() {
-  //rotation->refresh();
-  //esc->refresh();
   delay(1);
 }
 
